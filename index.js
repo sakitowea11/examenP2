39095fa73d16c62acc13139d67e2ce7ccfc4c21a
sakitowea11 <carugel1@espe.edu.ec>
2022-01-24 11:52:08 -0500

    Examen P2

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+//const res =require("espress/lib/response");
+const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+const port = process.env.port || 3000;
+
+app.use(express.static(__dirname+'/public'));
+
+//Motor de vistas HBS de express
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+app.get('/', (req,res)=>{
+    res.render("home",{
+     
+    });
+});
+
+app.listen(port,()=>{ 
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+})
+
+
